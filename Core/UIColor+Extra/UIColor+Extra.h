d142592825905b84951080c4d005af5cbbe86031
xinyzhao <xinyzhao@qq.com>
2016-12-06 14:13:15 +0800

    Add comments

--- Core/UIColor+Extra/UIColor+Extra.h
+++ Core/UIColor+Extra/UIColor+Extra.h
@@ -27,58 +27,151 @@
 #import <UIKit/UIKit.h>
 
 /**
- * Make color from integer with ARGB format.
- * eg. 0xffffffff or 0xffffff or 0xff etc.
+ Make color from integer with ARGB format.
+ eg. 0xffffffff or 0xffffff or 0xff etc.
+ 
+ @param value Color integer, ARGB format
+ @return UIColor
  */
 UIKIT_EXTERN UIColor * UIColorFromInteger(NSUInteger value);
+
+/**
+ Make color from RGBA value.
+
+ @param r red, 0 - 255
+ @param g green, 0 - 255
+ @param b blue, 0 - 255
+ @param a alpha, 0 - 255
+ @return UIColor
+ */
 UIKIT_EXTERN UIColor * UIColorFromRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+/**
+ Make color from RGB value, alpha is 255.
+
+ @param r red, 0 - 255
+ @param g green, 0 - 255
+ @param b blue, 0 - 255
+ @return UIColor
+ */
 UIKIT_EXTERN UIColor * UIColorFromRGB(uint8_t r, uint8_t g, uint8_t b);
 
 /**
- * Make color from HEX string with ARGB format.
- * eg. @"ffffffff" or @"#ffffff" or @"0xffffffff" etc.
+ Make color from HEX string
+ eg. @"ffffffff" or @"#ffffff" or @"0xffffffff" etc.
+ 
+ @param string HEX string, ARGB format
+ @return UIColor
  */
 UIKIT_EXTERN UIColor * UIColorFromHexString(NSString *string);
 
 /**
- * Make HEX string from color.
+ Make HEX string from color.
+
+ @param color UIColor
+ @param alpha Whether or not have alpha field
+ @return NSString
  */
 UIKIT_EXTERN NSString * UIColorToHexString(UIColor *color, BOOL alpha);
 
 /**
- * Make an inverse color.
+ Inverse color
+
+ @param color Original color
+ @return UIColor
  */
 UIKIT_EXTERN UIColor * UIColorByInverse(UIColor *color);
 
 /**
- * UIColor Extra
+ UIColor Extra
  */
 @interface UIColor (Extra)
 
-/* Make color from HEX string with ARGB format. */
+/**
+ Make color with HEX string
+
+ @param string HEX string, ARGB format
+ @return UIColor
+ */
 + (instancetype)colorWithHexString:(NSString *)string;
+
+/**
+ Make color with HEX string and alpha
+
+ @param string HEX string, RGB format
+ @param alpha alpha value, 0.0 - 1.0
+ @return UIColor
+ */
 + (instancetype)colorWithHexString:(NSString *)string alpha:(CGFloat)alpha;
 
-/* Get random color */
+/**
+ Gen random color
+
+ @return UIColor
+ */
 + (UIColor *)randomColor;
 
-/* Return the HEX string of the color. */
+/**
+ Return the HEX string of the color
+
+ @return UIColor
+ */
 - (NSString *)hexString;
 
-/* Return the inverse color. */
+/**
+ Return the inverse color
+
+ @return UIColor
+ */
 - (UIColor *)inverseColor;
 
-/* Return the alpha value of the color. */
+/**
+ Return the alpha value of the color
+
+ @return Alpha value
+ */
 - (CGFloat)alpha;
 
-/* Return the RGB components of the color. */
+/**
+ Return the red value of the color
+
+ @return Red value
+ */
 - (CGFloat)red;
+
+/**
+ Return the green value of the color
+ 
+ @return Green value
+ */
 - (CGFloat)green;
+
+/**
+ Return the blue value of the color
+ 
+ @return Blue value
+ */
 - (CGFloat)blue;
 
-/* Return the HSB components of the color. */
+/**
+ Return the hue value of the color
+ 
+ @return Hue value
+ */
 - (CGFloat)hue;
+
+/**
+ Return the saturation value of the color
+ 
+ @return Saturation value
+ */
 - (CGFloat)saturation;
+
+/**
+ Return the brightness value of the color
+ 
+ @return Brightness value
+ */
 - (CGFloat)brightness;
 
 @end
